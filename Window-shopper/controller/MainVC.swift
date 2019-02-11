d8a3cec53110a6afe7b6da19ce0138bdea2c0fc2
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-11 17:46:46 +0530

    Input Accessory view and IBDesignable

--- Window-shopper/controller/MainVC.swift
+++ Window-shopper/controller/MainVC.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var hourlyWageTF: CustomTextField!
+    @IBOutlet weak var itemPriceTF: CustomTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let accessorryBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 50))
+        accessorryBtn.setTitle("Calculate", for: .normal)
+        accessorryBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        accessorryBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        accessorryBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        hourlyWageTF.inputAccessoryView = accessorryBtn
+        itemPriceTF.inputAccessoryView = accessorryBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate()
+    {
+        print("Calculate was pressed")
     }
+    
 
 
 }
--- Window-shopper/views/CustomTextField.swift
+++ Window-shopper/views/CustomTextField.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+@IBDesignable
 class CustomTextField: UITextField {
 
+    override func prepareForInterfaceBuilder()
+    {
+        customizeTF()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeTF()
+    }
+    
+    func customizeTF()
+    {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.4186108733)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -20,7 +30,7 @@ class CustomTextField: UITextField {
         {
             let place = NSAttributedString(string: p, attributes: [ .foregroundColor  : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         }
     }
 
